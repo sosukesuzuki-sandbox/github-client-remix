46fd1480aac7723a9778529e42171df65bb09213
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2022-11-20 02:05:52 +0900

    Switch sign_in

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,10 +1,21 @@
-import { NavLink } from "@remix-run/react";
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { NavLink, useLoaderData } from "@remix-run/react";
+import { getSession } from "~/session.server";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const session = await getSession(request.headers.get("Cookie"));
+  return json({
+    isSignedIn: Boolean(session.get("access_token")),
+  });
+};
 
 export default function Index() {
+  const { isSignedIn } = useLoaderData();
   return (
     <main>
       <h1>GitHub Client built with Remix!</h1>
-      <NavLink to="signin">Sign In</NavLink>
+      {isSignedIn ? null : <NavLink to="signin">Sign In</NavLink>}
     </main>
   );
 }
